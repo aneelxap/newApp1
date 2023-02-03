c0cb3d5f0637520334ef1efd56745b30e0ac6298
AneelAP <anilprajapati0049@gmail.com>
2023-02-03 13:45:50 +0530

    created package controller, beans, daos, config

---
+++ xyzportal/src/main/java/com/xyzportal/XyzportalApplication.java
@@ -0,0 +1,16 @@
+package com.xyzportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class XyzportalApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(XyzportalApplication.class, args);
+	}
+
+}
